54e810e3258cfc6569d15fe246acb01302bfacf1
Pascal Ruhl <pascal.ruhl@smail.th-koeln.de>
2025-09-08 21:32:01 +0200

    Water Maps

--- MCA/water_maps.py
+++ MCA/water_maps.py
@@ -28,17 +28,16 @@ gdf_cwb = gpd.read_file('Data/cwp_2050.shp')
 
 # Map Ground Water
 fig, ax = plt.subplots(figsize=(15, 10))
-# gdf_landuse_concat.plot(ax = ax, column='GRIDCODE', cmap='tab20', legend=True)
-gdf_groundwater.plot(ax = ax, color='blue',alpha = 0.5, label='Groundwater potential')
+gdf_groundwater.plot(ax = ax, color='steelblue',alpha = 0.6, label='Groundwater potential')
 gdf_morocco_boundary.plot(ax=ax, edgecolor='black', facecolor="none", linewidth=1)
 cx.add_basemap(ax, crs=gdf_morocco_boundary.crs, source=cx.providers.CartoDB.Positron)
-plt.title('Groundwater Morocco', fontsize = 15)
+plt.title('Groundwater Morocco', fontsize = 20)
 plt.axis('off')
 legend_handles = [
-    mpatches.Patch(color='blue', label='Aquifer Productivity \nbetween moderate and very high'),
+    mpatches.Patch(color='steelblue', label='Aquifer Productivity \nbetween moderate and very high'),
 ]
 plt.legend(handles=legend_handles, loc='lower right', fontsize=12)
-# plt.savefig("morocco_landuse.pdf", format="pdf", dpi=300)
+plt.savefig("Maps/morocco_groundwater.pdf", format="pdf", dpi=300)
 plt.show()
 
 # Map Surface Water
@@ -52,10 +51,9 @@ fig, ax = plt.subplots(figsize=(15, 10))
 gdf_grid.plot(ax = ax, column='Surface Water', cmap = new_cmap, label='Surface Water potential',legend = True, legend_kwds={"label": "Surface Water Availability [MCM]", "orientation": "vertical"})
 gdf_morocco_boundary.plot(ax=ax, edgecolor='black', facecolor="none", linewidth=1)
 cx.add_basemap(ax, crs=gdf_morocco_boundary.crs, source=cx.providers.CartoDB.Positron)
-plt.title('Availability of Surface Water Morocco', fontsize = 15)
+plt.title('Availability of Surface Water Morocco', fontsize = 20)
 plt.axis('off')
-# plt.legend(handles=legend_handles, loc='lower right', fontsize=12)
-# plt.savefig("morocco_landuse.pdf", format="pdf", dpi=300)
+plt.savefig("Maps/morocco_surfacewater.pdf", format="pdf", dpi=300)
 plt.show()
 
 # Map flow direction
@@ -63,22 +61,22 @@ fig, ax = plt.subplots(figsize=(15, 10))
 gdf_water_direction.plot(ax = ax, column='_sum', cmap = 'Blues', legend=True, legend_kwds={"label": "Number of cells flowing into one cell", "orientation": "vertical"})
 gdf_morocco_boundary.plot(ax=ax, edgecolor='black', facecolor="none", linewidth=1)
 cx.add_basemap(ax, crs=gdf_morocco_boundary.crs, source=cx.providers.CartoDB.Positron)
-plt.title('Flow direction Morocco', fontsize = 15)
+plt.title('Flow direction Morocco', fontsize = 20)
 plt.axis('off')
 cbar_ax = fig.axes[-1]
 cbar_ax.tick_params(labelsize=12)
 cbar_ax.yaxis.label.set_size(14)
-# plt.savefig("morocco_landuse.pdf", format="pdf", dpi=300)
+plt.savefig("Maps/morocco_flow_direction.pdf", format="pdf", dpi=300)
 plt.show()
 
-# Map Watershaeds
+# Map Watersheds
 fig, ax = plt.subplots(figsize=(15, 10))
-gdf_watershead_morocco.plot(ax = ax, column='name',alpha=0.7,edgecolor='black', cmap = 'Paired', legend=True, legend_kwds={'loc': 'lower right'})
+gdf_watershead_morocco.plot(ax = ax, column='name',alpha=0.5,edgecolor='black', cmap = 'Paired', legend=True, legend_kwds={'loc': 'lower right'})
 gdf_morocco_boundary.plot(ax=ax, edgecolor='black', facecolor="none", linewidth=1)
 cx.add_basemap(ax, crs=gdf_morocco_boundary.crs, source=cx.providers.CartoDB.Positron)
-plt.title('Watersheds Morocco', fontsize = 15)
+plt.title('Watersheds Morocco', fontsize = 20)
 plt.axis('off')
-# plt.savefig("morocco_landuse.pdf", format="pdf", dpi=300)
+plt.savefig("Maps/morocco_watersheds.pdf", format="pdf", dpi=300)
 plt.show()
 
 # Map CWB
@@ -86,10 +84,10 @@ fig, ax = plt.subplots(figsize=(15, 10))
 gdf_cwb.plot(ax = ax, column='CWB',alpha=0.9, cmap = 'Blues', legend=True, legend_kwds={"label": "Climate Water Balance [MCM]", "orientation": "vertical"})
 gdf_morocco_boundary.plot(ax=ax, edgecolor='black', facecolor="none", linewidth=1)
 cx.add_basemap(ax, crs=gdf_morocco_boundary.crs, source=cx.providers.CartoDB.Positron)
-plt.title('Climate Water Balance Morocco', fontsize = 15)
+plt.title('Climate Water Balance Morocco between 2025 and 2050', fontsize = 20)
 plt.axis('off')
 cbar_ax = fig.axes[-1]
 cbar_ax.tick_params(labelsize=12)
 cbar_ax.yaxis.label.set_size(14)
-# plt.savefig("morocco_landuse.pdf", format="pdf", dpi=300)
+plt.savefig("Maps/morocco_cwb.pdf", format="pdf", dpi=300)
 plt.show()
