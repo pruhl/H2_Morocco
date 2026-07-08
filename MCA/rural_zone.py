3bb0b695a76a8563ec004f1b5552ac8a3ec4f2d9
Pascal Ruhl <pascal.ruhl@smail.th-koeln.de>
2026-07-08 17:26:30 +0200

    rural and urban

--- MCA/rural_zone.py
+++ MCA/rural_zone.py
@@ -11,12 +11,12 @@ gdf_grid = gpd.read_file('Grid_morocco/grid_morocco_clear.shp')
     # Source: FAO, via: https://data.apps.fao.org/catalog/dataset/fad3f475-8973-463f-b56a-e6b6535c1db5  --> Morocco
     # Source: FAO, via: https://data.apps.fao.org/catalog/iso/75aaf5c5-d579-425a-97fb-aa4580536df2      --> Western Sahara
 
-# gdf_landuse_mar         = gpd.read_file(r'C:\Users\psclr\Downloads\geonetwork_landcover_mar_gc_adg\mar_gc_adg.shp').to_crs("EPSG:32629") 
-# gdf_landuse_wsa         = gpd.read_file(r'C:\Users\psclr\Downloads\geonetwork_landcover_wsa_gc_adg\wsa_gc_adg.shp').to_crs("EPSG:32629")
-# gdf_landuse_concat      = gpd.GeoDataFrame(pd.concat([gdf_landuse_mar, gdf_landuse_wsa]))       #Today
+gdf_landuse_mar         = gpd.read_file(r'C:\Users\psclr\Documents\02 Master\Masterprojekt\QGIS\Daten\mar_gc_adg\mar_gc_adg.shp').to_crs("EPSG:32629") 
+gdf_landuse_wsa         = gpd.read_file(r'C:\Users\psclr\Documents\02 Master\Masterprojekt\QGIS\Daten\wsa_gc_adg\wsa_gc_adg.shp').to_crs("EPSG:32629")
+gdf_landuse_concat      = gpd.GeoDataFrame(pd.concat([gdf_landuse_mar, gdf_landuse_wsa]))       #Today
 
     # Same source, but for future landuse (from forecast_landuse.py)
-gdf_landuse_concat      = gpd.read_file('Data/morocco_landuse_2050.shp')    #Landuse 2050
+gdf_landuse_concat_2050      = gpd.read_file('Data/morocco_landuse_2050.shp')    #Landuse 2050
 
 gdf_landuse_rural       = gdf_landuse_concat[gdf_landuse_concat['GRIDCODE'].isin([200, 201, 202, 203, 220, 230, 
                                                                                   40, 41,42,50,60,70,90,91,92,
@@ -24,17 +24,28 @@ gdf_landuse_rural       = gdf_landuse_concat[gdf_landuse_concat['GRIDCODE'].isin
                                                                                   140,141,150,151,152,160,161,
                                                                                   162,170,180,181,185])]
 
-array_rural = np.array([])
+gdf_landuse_rural_2050       = gdf_landuse_concat_2050[gdf_landuse_concat_2050['GRIDCODE'].isin([200, 201, 202, 203, 220, 230, 
+                                                                                  40, 41,42,50,60,70,90,91,92,
+                                                                                  100,101,110,120,130,131,134,
+                                                                                  140,141,150,151,152,160,161,
+                                                                                  162,170,180,181,185])]
+
+list_rural = []
+list_rural_2050 = []
 for i in range(len(gdf_grid)):
     cell, list_index_intersection = list_index(gdf_landuse_rural, i, gdf_grid)
+    cell_2050, list_index_intersection_2050 = list_index(gdf_landuse_rural_2050, i, gdf_grid)
 
     area = gdf_landuse_rural.loc[list_index_intersection].intersection(cell).area.sum()
-    array_rural = np.append(array_rural, area/cell.area)
+    area_2050 = gdf_landuse_rural_2050.loc[list_index_intersection_2050].intersection(cell_2050).area.sum()
+    list_rural.append(area/cell.area)
+    list_rural_2050.append(area_2050/cell_2050.area)
 
-df_rural = pd.DataFrame(data = array_rural)
+df_rural = pd.DataFrame(data = list_rural)
+df_rural_2050 = pd.DataFrame(data = list_rural_2050)
 
 # # 2025
-# df_rural.to_csv('Data/results_rural.csv', index=False)
+df_rural.to_csv('results/results_rural.csv', index=False)
 
 # 2050
-df_rural.to_csv('Data/results_rural_2050.csv', index=False)
+df_rural_2050.to_csv('results/results_rural_2050.csv', index=False)
--- MCA/urban_zone.py
+++ MCA/urban_zone.py
@@ -11,26 +11,34 @@ gdf_grid = gpd.read_file('Grid_morocco/grid_morocco_clear.shp')
     # Source: FAO, via: https://data.apps.fao.org/catalog/dataset/fad3f475-8973-463f-b56a-e6b6535c1db5  --> Morocco
     # Source: FAO, via: https://data.apps.fao.org/catalog/iso/75aaf5c5-d579-425a-97fb-aa4580536df2      --> Western Sahara
 
-# gdf_landuse_mar         = gpd.read_file(r'C:\Users\psclr\Downloads\geonetwork_landcover_mar_gc_adg\mar_gc_adg.shp').to_crs("EPSG:32629") 
-# gdf_landuse_wsa         = gpd.read_file(r'C:\Users\psclr\Downloads\geonetwork_landcover_wsa_gc_adg\wsa_gc_adg.shp').to_crs("EPSG:32629")
-# gdf_landuse_concat      = gpd.GeoDataFrame(pd.concat([gdf_landuse_mar, gdf_landuse_wsa]))   #Today
+gdf_landuse_mar         = gpd.read_file(r'C:\Users\psclr\Documents\02 Master\Masterprojekt\QGIS\Daten\mar_gc_adg\mar_gc_adg.shp').to_crs("EPSG:32629") 
+gdf_landuse_wsa         = gpd.read_file(r'C:\Users\psclr\Documents\02 Master\Masterprojekt\QGIS\Daten\wsa_gc_adg\wsa_gc_adg.shp').to_crs("EPSG:32629")
+gdf_landuse_concat      = gpd.GeoDataFrame(pd.concat([gdf_landuse_mar, gdf_landuse_wsa]))   #Today
 
     # Same source, but for future landuse (from forecast_landuse.py)
-gdf_landuse_concat      = gpd.read_file('Data/morocco_landuse_2050.shp')    #Landuse 2050
+gdf_landuse_concat_2050      = gpd.read_file('Data/morocco_landuse_2050.shp')    #Landuse 2050
 
 gdf_landuse_urban       = gdf_landuse_concat[gdf_landuse_concat['GRIDCODE'].isin([190])]
+gdf_landuse_urban_2050  = gdf_landuse_concat_2050[gdf_landuse_concat_2050['GRIDCODE'].isin([190])]
 
-array_urban = np.array([])
+list_urban = []
+list_urban_2050 = []
 for i in range(len(gdf_grid)):
     cell, list_index_intersection = list_index(gdf_landuse_urban, i, gdf_grid)
     area = gdf_landuse_urban.loc[list_index_intersection].intersection(cell).area.sum()
 
-    array_urban = np.append(array_urban, area/cell.area)
+    cell_2050, list_index_intersection_2050 = list_index(gdf_landuse_urban_2050, i, gdf_grid)
+    area_2050 = gdf_landuse_urban_2050.loc[list_index_intersection_2050].intersection(cell_2050).area.sum()
+    
+    
+    list_urban.append(area/cell.area)
+    list_urban_2050.append(area_2050/cell_2050.area)
 
-df_urban = pd.DataFrame(data = array_urban)
+df_urban = pd.DataFrame(data = list_urban)
+df_urban_2050 = pd.DataFrame(data = list_urban_2050)
 
 # # 2025
-# df_urban.to_csv('Data/results_urban.csv', index=False)
+df_urban.to_csv('results/results_urban.csv', index=False)
 
 # 2050
-df_urban.to_csv('Data/results_urban_2050.csv', index=False)
+df_urban_2050.to_csv('results/results_urban_2050.csv', index=False)
